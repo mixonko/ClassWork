f069eea52292b695881d5271dbb1c5f9d4352e71
user <ijhkljh@jhgfgf.ru>
2019-03-12 20:35:15 +0300

    mvvm

--- app/src/main/java/com/pvt/cleanproject/presentacion/test/mvvm/TestMvvmActivity.kt
+++ app/src/main/java/com/pvt/cleanproject/presentacion/test/mvvm/TestMvvmActivity.kt
@@ -6,12 +6,29 @@ import com.pvt.cleanproject.R
 import kotlinx.android.synthetic.main.activity_test_mvp_mvvm.*
 
 class TestMvvmActivity: Activity() {
+    private lateinit var viewModel: MvvmViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_mvp_mvvm)
 
+        viewModel = TestMvvmViewModelDefault()
+
+        viewModel.setHelloTextListener(object :  HelloTextListener {
+            override fun onUpdate(value: String) {
+                helloTextView.text = value
+
+            }
+
+        })
+
         helloTextView.setOnClickListener {
 
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.reloadData()
     }
 }
